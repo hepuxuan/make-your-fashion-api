389f8980e4f00a190ec0ea03ac4c14e2dbba4bca
Puxuan <phe@expedia.com>
2017-07-16 12:56:15 -0500

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # Change these
-server 'your_server_ip', port: 9000, roles: [:web, :app, :db], primary: true
+server '59.110.141.38', port: 9000, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:hepuxuan/make-your-fashion-api.git'
 set :application,     'make-your-fashion-api'
